b808e9c36bf58d79c0baa8598e5fe4821612258a
Subho Yadav <subhoyadav79@gmail.com>
2023-11-15 12:27:13 +0530

    removed configs

--- client/src/cofigs/firebase.js
+++ client/src/cofigs/firebase.js
@@ -5,17 +5,7 @@ function initialiseFirebase() {
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDQd_D_5sPTyI8r7w4rVKNiN83nkLUM35c",
-    authDomain: "playground-675cd.firebaseapp.com",
-    databaseURL:
-      "https://playground-675cd-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "playground-675cd",
-    storageBucket: "playground-675cd.appspot.com",
-    messagingSenderId: "96829841844",
-    appId: "1:96829841844:web:5c49bae7a6fd91558952e1",
-  };
+  // Your web app's Firebase configuration goes here
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
